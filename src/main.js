b40b429f42ae813c88c9ece9c282edd1c6ad341e
Bogdan Dikhtiar <bogdandihtyar@gmail.com>
2025-05-27 17:38:58 +0300

    user registration

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App'
 import router from './router'
 import store from './store'
 import Vuetify from 'vuetify'
+import * as fb from 'firebase'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
@@ -15,5 +16,15 @@ new Vue({
   router,
   store,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  created () {
+    fb.initializeApp({
+      apiKey: 'AIzaSyBnsQcM0DladVfKhMb3Mu_uROpJp68tgas',
+      authDomain: 'adds-project-2a7c9.firebaseapp.com',
+      projectId: 'adds-project-2a7c9',
+      storageBucket: 'adds-project-2a7c9.firebasestorage.app',
+      messagingSenderId: '942128027123',
+      appId: '1:942128027123:web:0d5fb9b87e4e924b1b6e86'
+    })
+  }
 })
